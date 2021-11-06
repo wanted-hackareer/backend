e66db62877e3277ec257e4f72e18158d3d45cbcf
chungchung <61677941+piaochung@users.noreply.github.com>
2021-11-06 19:49:20 +0900

    Test: "추가 createChat"

---
+++ core/src/test/java/backend/core/domain/ChatTest.java
@@ -0,0 +1,24 @@
+package backend.core.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ChatTest {
+
+    @Test
+    @DisplayName("Chat 생성")
+    public void createChat() {
+        //given
+        Address address = Address.builder().city("서울시").district("테스트").street("성동로").build();
+        Member author = Member.builder().email("haha@gmail.com").password("DF#Q$FWAD").address(address).nickName("와바라바덥덥").build();
+        Post post = Post.builder().member(author).description("테스트 설명글").title("테스트 제목").maximum(3).build();
+
+        //when
+        Chat chat = Chat.builder().post(post).build();
+
+        //then
+        assertThat(chat.getPost()).isEqualTo(post);
+    }
+}

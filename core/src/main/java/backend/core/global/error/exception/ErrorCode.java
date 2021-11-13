fe067a211bccf51755fad8034d0813ada855ccab
chungchung <61677941+piaochung@users.noreply.github.com>
2021-11-13 13:18:19 +0900

    Feat: "추가 ErrorCode"

--- core/src/main/java/backend/core/global/error/exception/ErrorCode.java
+++ core/src/main/java/backend/core/global/error/exception/ErrorCode.java
@@ -16,11 +16,15 @@ public enum ErrorCode {
     /* 401 UNAUTHORIZED : 인증되지 않은 사용자 */
     UNAUTHORIZED_USER(UNAUTHORIZED, "인증되지 않은 사용자입니다"),
 
+    /* 407 NOT_ACCEPTABLE : 허용되지 않는 접근 */
+    MEMBER_NOT_ACCEPTABLE(NOT_ACCEPTABLE, "작성자만 수정할 수 있습니다"),
+
     /* 404 NOT_FOUND : Resource 를 찾을 수 없음 */
     RATING_INFO_NOT_FOUND(NOT_FOUND, "평가 내용을 찾을 수 없습니다"),
     RATING_NOT_FOUND(NOT_FOUND, "사용자 평가를 찾을 수 없습니다"),
     MEMBER_NOT_FOUND(NOT_FOUND, "사용자를 찾을 수 없습니다"),
     CHAT_NOT_FOUND(NOT_FOUND, "채팅방을 찾을 수 없습니다"),
+    POST_NOT_FOUND(NOT_FOUND, "게시글을 찾을 수 없습니다"),
 
     /* 409 CONFLICT : Resource 의 현재 상태와 충돌. 보통 중복된 데이터 존재 */
     DUPLICATE_RESOURCE(CONFLICT, "데이터가 이미 존재합니다"),

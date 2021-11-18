4929a9abbd8e528aee1c5040ad32bc8fd4c58ef9
chungchung <61677941+piaochung@users.noreply.github.com>
2021-11-18 10:52:11 +0900

    Test: "추가 TagServiceTest [create, findAll]"

--- core/src/test/java/backend/core/service/TagServiceTest.java
+++ core/src/test/java/backend/core/service/TagServiceTest.java
@@ -1,5 +1,65 @@
 package backend.core.service;
 
+import backend.core.domain.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static backend.core.dto.request.TagRequestDto.TagCreateRequestDto;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
 public class TagServiceTest {
 
+    @Autowired
+    private TagService tagService;
+
+    @Autowired
+    private EntityManager em;
+
+    @Test
+    @DisplayName("Tag 생성")
+    public void create() {
+        //given
+        Address address = Address.builder().city("서울시").district("강동구").street("미아로").build();
+        Profile profile = Profile.builder().storeFileName("ASDAS-asDASDAS-aSDSA.jpg").uploadFileName("프로필 이미지").build();
+        Member member = Member.builder().email("test1@gmail.com").password("DF#Q$FWAD").address(address).nickName("테스트1").profile(profile).build();
+        Post post = Post.builder().title("테스트 제목").description("테스트 본문").member(member).dayOfTheWeek("월, 금").maximum(2).build();
+        em.persist(post);
+
+        //when
+        TagCreateRequestDto dto = new TagCreateRequestDto(post.getId(), "아이언맨");
+        dto.setPost(post);
+        Long tagId = tagService.save(dto);
+
+        //then
+        assertThat(tagService.findById(tagId)).isInstanceOf(Tag.class);
+        assertThat(tagService.findById(tagId).getName()).isEqualTo("아이언맨");
+        assertThat(tagService.findById(tagId).getPost().getId()).isEqualTo(post.getId());
+    }
+
+    @Test
+    @DisplayName("Tag 조회 - findAll")
+    public void findAll() {
+        //given
+        Address address = Address.builder().city("서울시").district("강동구").street("미아로").build();
+        Profile profile = Profile.builder().storeFileName("ASDAS-asDASDAS-aSDSA.jpg").uploadFileName("프로필 이미지").build();
+        Member member = Member.builder().email("test1@gmail.com").password("DF#Q$FWAD").address(address).nickName("테스트1").profile(profile).build();
+        Post post = Post.builder().title("테스트 제목").description("테스트 본문").member(member).dayOfTheWeek("월, 금").maximum(2).build();
+        em.persist(post);
+
+        //when
+        Tag tagA = Tag.builder().post(post).name("아이언맨").build();
+        Tag tagB = Tag.builder().post(post).name("캡틴 아메리카").build();
+        em.persist(tagA);
+        em.persist(tagB);
+
+        //then
+        assertThat(tagService.findAll(0, 100).size()).isEqualTo(2);
+    }
 }

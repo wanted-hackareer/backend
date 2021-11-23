ca9ba9e09aa9118ba1451efe7e8c6d8fcc287677
chungchung <61677941+piaochung@users.noreply.github.com>
2021-11-24 07:14:09 +0900

    Teat: "추가 PostController save 함수"

---
+++ core/src/test/java/backend/core/controller/ApiDocumentationTest.java
@@ -0,0 +1,46 @@
+package backend.core.controller;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.persistence.EntityManager;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public abstract class ApiDocumentationTest {
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    protected WebApplicationContext context;
+
+    @Autowired
+    protected EntityManager em;
+
+    @Before
+    public void setUp() {
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(documentationConfiguration(restDocumentation))
+                .build();
+    }
+}
---
+++ core/src/test/java/backend/core/controller/PostControllerTest.java
@@ -0,0 +1,72 @@
+package backend.core.controller;
+
+import backend.core.domain.Address;
+import backend.core.domain.Basket;
+import backend.core.domain.Member;
+import backend.core.domain.Profile;
+import backend.core.dto.request.PostRequestDto;
+import backend.core.dto.response.StaffResponseDto;
+import backend.core.dto.response.TagResponseDto;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+
+import static backend.core.dto.request.PostRequestDto.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@Transactional
+public class PostControllerTest extends ApiDocumentationTest {
+
+    @Autowired
+    private PostController postController;
+
+    @Test
+    @DisplayName("post 저장 (api)")
+    public void save() throws Exception {
+        //given
+        Address address = Address.builder().city("서울시").district("강동구").street("미아로").build();
+        Profile profile = Profile.builder().storeFileName("ASDAS-asDASDAS-aSDSA.jpg").uploadFileName("프로필 이미지").build();
+        Basket basket = Basket.builder().build();
+        Member member = Member.builder().email("test@gmail.com").password("DF#Q$FWAD").address(address).nickName("테스트2").basket(basket).profile(profile).build();
+        em.persist(member);
+
+        PostCreateRequestDto dto = new PostCreateRequestDto("테스트 제목", "테스트 본문", member.getId(), 3, "월, 화, 수");
+
+        //when
+        ResultActions result = mockMvc.perform(post("/api/v1/post")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(objectMapper.writeValueAsString(dto))
+                .accept(MediaType.APPLICATION_JSON));
+
+        //then
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value(dto.getTitle()))
+                .andExpect(jsonPath("$.author").value(member.getNickName()))
+                .andDo(document("post-생성",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                            fieldWithPath("title").type(JsonFieldType.STRING).description("게시글 제목"),
+                            fieldWithPath("description").type(JsonFieldType.STRING).description("게시글 설명글"),
+                            fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("작성 회원 id"),
+                            fieldWithPath("maximum").type(JsonFieldType.NUMBER).description("게시글 모집 인원"),
+                            fieldWithPath("dayOfTheWeek").type(JsonFieldType.STRING).description("가능한 요일 정보")
+                        )));
+    }
+}

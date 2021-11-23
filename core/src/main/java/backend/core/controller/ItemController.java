f756a3f52c127260706955051eb14ed3dadd7fb8
chungchung <61677941+piaochung@users.noreply.github.com>
2021-11-23 17:08:57 +0900

    Feat: "추가 ItemController Id 조회, 전체 조회, 업데이트 함수"

--- core/src/main/java/backend/core/controller/ItemController.java
+++ core/src/main/java/backend/core/controller/ItemController.java
@@ -3,20 +3,21 @@ package backend.core.controller;
 import backend.core.domain.Basket;
 import backend.core.domain.Item;
 import backend.core.dto.response.ItemResponseDto;
+import backend.core.global.response.ApiResponse;
 import backend.core.service.BasketService;
 import backend.core.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 import static backend.core.dto.request.ItemRequestDto.ItemCreateRequestDto;
+import static backend.core.dto.request.ItemRequestDto.ItemUpdateRequestDto;
 
-@RestController @Slf4j
+@RestController
+@Slf4j
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class ItemController {
@@ -24,14 +25,38 @@ public class ItemController {
     private final ItemService itemService;
     private final BasketService basketService;
 
+    @GetMapping("/item/{id}")
+    public ItemResponseDto itemV1(
+            @PathVariable Long id) {
+        Item item = itemService.findByIdOrThrow(id);
+        return new ItemResponseDto(item);
+    }
+
+    @GetMapping("/items")
+    public ApiResponse itemsV1(
+            @RequestParam(name = "offset", defaultValue = "0") int offset,
+            @RequestParam(name = "limit", defaultValue = "100") int limit) {
+        List<Item> items = itemService.findAllOrThrow(offset, limit);
+
+        return ApiResponse.builder().count(items.size()).data(items).build();
+    }
+
     @PostMapping("/item")
-    public ItemResponseDto save(
+    public ItemResponseDto saveItemV1(
             @Valid @RequestBody ItemCreateRequestDto dto) {
         Basket basket = basketService.findByIdOrThrow(dto.getBasketId());
         dto.setBasket(basket);
 
         Long itemId = itemService.save(dto);
         Item item = itemService.findByIdOrThrow(itemId);
+        return new ItemResponseDto(item);
+    }
+
+    @PutMapping("/item")
+    public ItemResponseDto updateItemV1(
+            @Valid @RequestBody ItemUpdateRequestDto dto) {
+        Long id = itemService.update(dto);
+        Item item = itemService.findByIdOrThrow(id);
         return new ItemResponseDto(item);
     }
 }

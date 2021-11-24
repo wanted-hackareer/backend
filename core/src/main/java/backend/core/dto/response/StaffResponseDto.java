5230d643eb402648adfa83bcd6da616b1bdea724
chungchung <61677941+piaochung@users.noreply.github.com>
2021-11-25 00:29:12 +0900

    Feat: "추가 memberId"

--- core/src/main/java/backend/core/dto/response/StaffResponseDto.java
+++ core/src/main/java/backend/core/dto/response/StaffResponseDto.java
@@ -9,12 +9,14 @@ import java.time.LocalDateTime;
 @Getter
 public class StaffResponseDto {
     private Long id;
+    private Long memberId;
     private StaffStatus status;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
     public StaffResponseDto(Staff entity) {
         id = entity.getId();
+        memberId = entity.getMember().getId();
         status = entity.getStaffStatus();
         createdAt = entity.getCreatedAt();
         updatedAt = entity.getUpdatedAt();

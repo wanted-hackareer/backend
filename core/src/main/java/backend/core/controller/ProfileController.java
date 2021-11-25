9ee97591da156b1e1a342987e0ec2baf4d577832
chungchung <61677941+piaochung@users.noreply.github.com>
2021-11-25 14:53:44 +0900

    Feat: "추가 profile 저장 시 회원의 프로필 정보 수정"

--- core/src/main/java/backend/core/controller/ProfileController.java
+++ core/src/main/java/backend/core/controller/ProfileController.java
@@ -1,10 +1,13 @@
 package backend.core.controller;
 
 import backend.core.domain.Member;
+import backend.core.domain.Profile;
+import backend.core.dto.request.MemberRequestDto;
 import backend.core.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -23,25 +26,40 @@ public class ProfileController {
 
     private final MemberService memberService;
 
-    @GetMapping("/profile/{id}")
-    public String profileV1(@PathVariable Long id) {
-        Member member = memberService.findByIdOrThrow(id);
+    @GetMapping("/profile/{memberId}")
+    public String profileV1(@PathVariable Long memberId) {
+        Member member = memberService.findByIdOrThrow(memberId);
         return member.getProfile().getStoreFileName();
     }
 
-    @PostMapping("/profile")
+    @PostMapping("/profile/{memberId}")
     public String saveProfile(
+            @PathVariable Long memberId,
             @RequestParam MultipartFile file) throws IOException {
         String storeFilename = createStoreFilename(file.getOriginalFilename());
-        log.info("saveProfile storeFilename = {}", storeFilename);
-        log.info("saveProfile originalFilename = {}", file.getOriginalFilename());
-
+        String fullPath = "";
         if (!file.isEmpty()) {
-            String fullPath = fileDir + storeFilename;
+            fullPath = fileDir + storeFilename;
             log.info("saveProfile fullPath = {}", fullPath);
             file.transferTo(new File(fullPath));
         }
-        return storeFilename;
+
+        Profile profile = createProfile(file, fullPath);
+        findMemberAndUpdateProfile(memberId, profile);
+
+        log.info("saveProfile storeFilename = {}, originalFilename = {}, fullPath = {}", storeFilename, file.getOriginalFilename(), fullPath);
+        return HttpStatus.OK.toString();
+    }
+
+    private void findMemberAndUpdateProfile(Long memberId, Profile profile) {
+        Member member = memberService.findByIdOrThrow(memberId);
+        MemberRequestDto.MemberUpdateRequestDto dto = new MemberRequestDto.MemberUpdateRequestDto(member.getId(), profile, member.getNickName(), member.getAddress());
+        memberService.updateOrThrow(dto);
+    }
+
+    private Profile createProfile(MultipartFile file, String fullPath) {
+        Profile profile = Profile.builder().uploadFileName(file.getOriginalFilename()).storeFileName(fullPath).build();
+        return profile;
     }
 
     private String createStoreFilename(String originalFilename) {

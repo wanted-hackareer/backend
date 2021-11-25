89501c4c07413f3dae6dd616e3ec879ad5e277d6
chungchung <61677941+piaochung@users.noreply.github.com>
2021-11-26 02:25:52 +0900

    Test: "추가 BasketServiceTest 생성, 조회 테스트"

--- core/src/test/java/backend/core/service/BasketServiceTest.java
+++ core/src/test/java/backend/core/service/BasketServiceTest.java
@@ -1,5 +1,45 @@
 package backend.core.service;
 
+import backend.core.domain.Basket;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
 public class BasketServiceTest {
 
+    @Autowired
+    private BasketService basketService;
+
+    @Test
+    @DisplayName("basket 생성")
+    public void save() {
+        //given
+        Long id = basketService.save();
+
+        //when
+        Basket basket = basketService.findByIdOrThrow(id);
+
+        //then
+        assertThat(basket.getId()).isEqualTo(id);
+        assertThat(basket).isInstanceOf(Basket.class);
+    }
+
+    @Test
+    @DisplayName("basket 조회 - findAll")
+    public void findAll() {
+        //given
+        Long idA = basketService.save();
+        Long idB = basketService.save();
+
+        //when
+
+        //then
+        assertThat(basketService.findAllOrThrow(0, 100).size()).isEqualTo(2);
+    }
 }

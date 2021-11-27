115b0b2c0fdc0853d7cf408144ce7710a31fb565
chungchung <61677941+piaochung@users.noreply.github.com>
2021-11-27 17:19:56 +0900

    Feat: "추가 TagInfoResponseDto"

--- core/src/main/java/backend/core/dto/response/TagResponseDto.java
+++ core/src/main/java/backend/core/dto/response/TagResponseDto.java
@@ -3,13 +3,30 @@ package backend.core.dto.response;
 import backend.core.domain.Tag;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class TagResponseDto {
     private Long id;
     private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public TagResponseDto(Tag entity) {
         id = entity.getId();
         name = entity.getName();
+        createdAt = entity.getCreatedAt();
+        updatedAt = entity.getUpdatedAt();
+    }
+
+    @Getter
+    public static class TagInfoResponseDto {
+        private Long id;
+        private String name;
+
+        public TagInfoResponseDto(Tag entity) {
+            id = entity.getId();
+            name = entity.getName();
+        }
     }
 }

c6293ddc8f283629447411c94a9aa8a76b248fab
chungchung <61677941+piaochung@users.noreply.github.com>
2021-11-27 17:18:26 +0900

    Feat: "추가 BasketInfoResponseDto"

--- core/src/main/java/backend/core/dto/response/BasketResponseDto.java
+++ core/src/main/java/backend/core/dto/response/BasketResponseDto.java
@@ -7,20 +7,35 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static backend.core.dto.response.ItemResponseDto.ItemInfoResponseDto;
+
 @Getter
 public class BasketResponseDto {
     private Long id;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     // TODO: itemList 추가
-    private List<ItemResponseDto> items;
+    private List<ItemResponseDto> itemList;
 
     public BasketResponseDto(Basket entity) {
         id = entity.getId();
         createdAt = entity.getCreatedAt();
         updatedAt = entity.getUpdatedAt();
-        items = entity.getItemList().stream()
+        itemList = entity.getItemList().stream()
                 .map(item -> new ItemResponseDto(item))
                 .collect(Collectors.toList());
+    }
+
+    @Getter
+    public static class BasketInfoResponseDto {
+        private Long id;
+        private List<ItemInfoResponseDto> itemList;
+
+        public BasketInfoResponseDto(Basket entity) {
+            id = entity.getId();
+            itemList = entity.getItemList().stream()
+                    .map(item -> new ItemInfoResponseDto(item))
+                    .collect(Collectors.toList());
+        }
     }
 }

a639b432c665f98c9f5a3737ca114eda45dc051f
chungchung <61677941+piaochung@users.noreply.github.com>
2021-11-27 17:21:16 +0900

    Feat: "추가 PostInfoResponseDto"

--- core/src/main/java/backend/core/dto/response/PostResponseDto.java
+++ core/src/main/java/backend/core/dto/response/PostResponseDto.java
@@ -40,4 +40,19 @@ public class PostResponseDto {
                 .map(tag -> new TagResponseDto(tag))
                 .collect(Collectors.toList());
     }
+
+    @Getter
+    public static class PostInfoResponseDto {
+        private Long id;
+        private String title;
+        private String description;
+        private String dayOfTheWeek;
+
+        public PostInfoResponseDto(Post entity) {
+            id = entity.getId();
+            title = entity.getTitle();
+            description = entity.getDescription();
+            dayOfTheWeek = entity.getDayOfTheWeek();
+        }
+    }
 }

c06e02bde77532e608d92fa98fbc3bf99196e897
chungchung <61677941+piaochung@users.noreply.github.com>
2021-12-21 23:34:11 +0900

    Feat: "추가 채팅방 생성 및 확인 기능"

--- core/src/main/java/backend/core/controller/ChatController.java
+++ core/src/main/java/backend/core/controller/ChatController.java
@@ -1,9 +1,21 @@
 package backend.core.controller;
 
+import backend.core.domain.Chat;
+import backend.core.domain.Post;
+import backend.core.dto.response.ChatResponseDto;
+import backend.core.service.ChatService;
+import backend.core.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.UUID;
+
+import static backend.core.dto.request.ChatRequestDto.ChatCreateRequestDto;
 
 @Slf4j
 @RestController
@@ -11,4 +23,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class ChatController {
 
+    private final PostService postService;
+    private final ChatService chatService;
+
+    @PostMapping("/chat")
+    public ChatResponseDto chatV1(
+            @Valid @RequestBody ChatCreateRequestDto dto) {
+        Post post = postService.findByIdOrThrow(dto.getPostId());
+        dto.setPostAndChatAddress(post, UUID.randomUUID().toString());
+
+        Long id = chatService.save(dto);
+        Chat chat = chatService.findByIdOrThrow(id);
+        return new ChatResponseDto(chat);
+    }
 }
--- core/src/main/java/backend/core/controller/MessageController.java
+++ core/src/main/java/backend/core/controller/MessageController.java
@@ -4,6 +4,7 @@ import backend.core.domain.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
 
     @MessageMapping("/message")
-    @SendTo("/topic/chat")
-    public Message broadCasting(Message message) {
-        log.info("message input = {}", message.getContent());
+    @SendTo("/topic/chat/{id}")
+    public Message broadCasting(
+            Message message,
+            @PathVariable String chatId) {
+        log.info("message input = {}, chatId = {}", message.getContent(), chatId);
         return message;
     }
 }

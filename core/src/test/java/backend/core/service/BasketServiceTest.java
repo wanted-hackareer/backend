f03f221357ff537731b76a7d523d1f4afd4c5f66
chungchung <61677941+piaochung@users.noreply.github.com>
2021-12-23 15:59:45 +0900

    Test: "수정 junit.Test -> junit.jupiter.api.Test"

--- core/src/test/java/backend/core/service/BasketServiceTest.java
+++ core/src/test/java/backend/core/service/BasketServiceTest.java
@@ -1,7 +1,7 @@
 package backend.core.service;
 
 import backend.core.domain.Basket;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;

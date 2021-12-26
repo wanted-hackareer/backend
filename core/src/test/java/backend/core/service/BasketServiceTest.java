6967b597ac9a579cb72861256941748d762a3eeb
chungchung <61677941+piaochung@users.noreply.github.com>
2021-12-26 18:12:04 +0900

    Test: "추가 basket 삭제 api 테스트"

--- core/src/test/java/backend/core/service/BasketServiceTest.java
+++ core/src/test/java/backend/core/service/BasketServiceTest.java
@@ -1,17 +1,26 @@
 package backend.core.service;
 
 import backend.core.domain.Basket;
+import backend.core.dto.request.ItemRequestDto;
+import backend.core.global.error.exception.CustomException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+
+import static backend.core.dto.request.ItemRequestDto.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
 public class BasketServiceTest {
+
+    @Autowired
+    private ItemService itemService;
 
     @Autowired
     private BasketService basketService;
@@ -41,5 +50,24 @@ public class BasketServiceTest {
 
         //then
         assertThat(basketService.findAllOrThrow(0, 100).size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("basket 삭제 - delete")
+    public void deleteBasket() {
+        //given
+        Long id = basketService.save();
+        ItemCreateRequestDto dtoA = new ItemCreateRequestDto(id, "사과", 3);
+        ItemCreateRequestDto dtoB = new ItemCreateRequestDto(id, "물 1.5L", 3);
+        itemService.save(dtoA);
+        itemService.save(dtoB);
+
+        //when
+        basketService.deleteById(id);
+
+        //then
+        Assertions.assertThrows(CustomException.class, () -> {
+            basketService.findByIdOrThrow(id);
+        });
     }
 }

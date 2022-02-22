d87a56dde2ed804fe35a0fcba7ac74cff1809fd9
chungchung <61677941+piaochung@users.noreply.github.com>
2022-02-22 21:59:38 +0900

    Delete: "삭제"

--- core/src/main/java/backend/core/global/error/exception/group/MessageNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package backend.core.global.error.exception.group;
-
-import backend.core.global.error.exception.CustomException;
-
-public class MessageNotFoundException extends CustomException {
-}

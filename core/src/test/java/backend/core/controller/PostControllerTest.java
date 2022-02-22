f48b591d8d7e640bb7d98b5ac8f428d3c817aa8d
chungchung <61677941+piaochung@users.noreply.github.com>
2022-02-23 01:24:50 +0900

    Test: "추가 PostController findById 테스트 코드"

--- core/src/test/java/backend/core/controller/PostControllerTest.java
+++ core/src/test/java/backend/core/controller/PostControllerTest.java
@@ -5,10 +5,10 @@ import backend.core.global.domain.Profile;
 import backend.core.member.domain.Member;
 import backend.core.member.dto.MemberSignUpRequestDto;
 import backend.core.member.service.MemberService;
-import backend.core.post.PostController;
 import backend.core.post.dto.PostCreateRequestDto;
+import backend.core.post.service.PostService;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -17,6 +17,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
@@ -29,14 +30,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class PostControllerTest extends ApiDocumentationTest {
 
     @Autowired
-    private PostController postController;
+    private PostService postService;
 
     @Autowired
     private MemberService memberService;
 
     private Member member;
 
-    @BeforeEach
+    @Before // @BeforeEach -> Junit5
     public void init() {
         Address address = Address.builder().city("서울시").district("강동구").street("미아로").build();
         Profile profile = Profile.builder().storeFileName("ASDAS-asDASDAS-aSDSA.jpg").uploadFileName("프로필 이미지").build();
@@ -55,6 +56,7 @@ public class PostControllerTest extends ApiDocumentationTest {
         ResultActions result = mockMvc.perform(post("/api/v1/post")
                 .contentType(MediaType.APPLICATION_JSON)
                 .characterEncoding("UTF-8")
+                .content("memberId:" + member.getId())
                 .content(objectMapper.writeValueAsString(dto))
                 .accept(MediaType.APPLICATION_JSON));
 
@@ -75,4 +77,26 @@ public class PostControllerTest extends ApiDocumentationTest {
     }
 
     //TODO 전체 조회, id 조회, 수정, 검색 테스트
+    @Test
+    @DisplayName("[api] post 조회")
+    public void findById() throws Exception {
+        //given
+        PostCreateRequestDto dto = new PostCreateRequestDto("테스트 제목", "테스트 본문", 3, "월, 화, 수");
+        Long postId = postService.save(dto, member.getId());
+
+        //when
+        ResultActions result = mockMvc.perform(get("/api/v1/post/" + postId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8"));
+
+        //then
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(postId))
+                .andExpect(jsonPath("$.author").value(member.getNickName()))
+                .andExpect(jsonPath("$.title").value(dto.getTitle()))
+                .andExpect(jsonPath("$.dayOfTheWeek").value(dto.getDayOfTheWeek()))
+                .andDo(document("post-id로 조회",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+    }
 }

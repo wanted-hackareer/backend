7cfb33da43ce009f3bcaf8df4df9d010c9ec3a98
chungchung <61677941+piaochung@users.noreply.github.com>
2022-02-26 12:11:41 +0900

    Feat: "추가 aop 사용하여 함수 호출 전후로 로그 찍기" (#6)

--- core/src/main/java/backend/core/CoreApplication.java
+++ core/src/main/java/backend/core/CoreApplication.java
@@ -1,17 +1,18 @@
 package backend.core;
 
+import backend.core.global.aop.AopConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@Import(AopConfig.class)
 public class CoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
 
 }
